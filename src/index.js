0b0c8a8a856166fe017a781955cdae4208f74949
LIVIO ANGELIM MORORO <lange70@wgu.edu>
2025-03-17 17:52:44 -0400

    Organize architecture - Move components to proper directories

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './styles/global.css';
-import ParcelForm from './components/ParcelForm/ParcelForm';
-import RouteDisplay from './components/RouteDisplay/RouteDisplay';
-
-document.addEventListener('DOMContentLoaded', () => {
-    new ParcelForm();
-    new RouteDisplay();
-});
